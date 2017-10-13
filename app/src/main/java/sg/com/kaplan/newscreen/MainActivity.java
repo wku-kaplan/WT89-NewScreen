cbe0d63d381041e79795411735f736ec5210b81f
William Ku <kucheech@gmail.com>
2017-10-13 22:33:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/newscreen/MainActivity.java
@@ -0,0 +1,30 @@
+package sg.com.kaplan.newscreen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
